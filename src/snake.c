f4a6949983b8206f6e1f2e127aa40155bc16ee56
nelsonsun <longphp@outlook.com>
2019-04-09 17:55:53 +0800

    include curses

--- src/snake.c
+++ src/snake.c
@@ -2,6 +2,7 @@
 // Created by nelsonsun on 19-3-18.
 //
 
+#include <curses.h>
 #include <stdio.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
@@ -10,13 +11,32 @@
 #include <string.h>
 
 #include "Map.h"
+#include "Food.h"
+
+#include <graphite2/Types.h>
+#include <termios.h>
+#include <term.h>
+
+/* implemented */
+
 
 int main()
 {
-    /** 初始化地图 **/
+    /* 初始化地图 **/
     initMap();
-    /** 显示地图 **/
+    /* 显示地图 **/
     map.show();
+    hello_world();
+
+    /* 初始化食物　**/
+    initFood();
+    for (int m = 0; m<MAX_FOOD; ++m)
+    {
+        food.show();
+        usleep(1000);
+    }
+
+    sleep(3);
     clearMap();
     return 0;
 }
