ef4497ea04fc7d13b54f3edc4cbdff449a4042f6
nelsonsun <longphp@outlook.com>
2019-04-09 17:52:21 +0800

    update

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ C_FILE=$(tree -f  src | egrep -o '[/a-zA-Z0-9]{1,}\.c$' | tr "\n" " ")
 echo $C_FILE
 
 # -g gdb   -lcurses ncurses 库
-`gcc -g $C_FILE  -lcurses -lm`
+`gcc -g $C_FILE  -lcurses -lm -o bin/snake`
